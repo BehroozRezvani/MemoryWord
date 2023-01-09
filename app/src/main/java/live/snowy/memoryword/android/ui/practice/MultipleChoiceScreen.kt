baf23d856c7e3ec19228fc4dafb3e3d8f9996422
Behrooz <brezvani@outlook.com>
2023-01-09 14:20:07 +0000

    Working on Practice pages

--- app/src/main/java/live/snowy/memoryword/android/ui/practice/MultipleChoiceScreen.kt
+++ app/src/main/java/live/snowy/memoryword/android/ui/practice/MultipleChoiceScreen.kt
@@ -3,30 +3,49 @@ package live.snowy.memoryword.android.ui.practice
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.magnifier
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import live.snowy.memoryword.android.model.Word
+import live.snowy.memoryword.android.model.WordsViewModel
 import live.snowy.memoryword.android.ui.theme.MemoryWordTheme
+
+@Composable
+fun FlashCardScreenTopLevel(
+    navController: NavHostController,
+    wordsViewModel: WordsViewModel = viewModel(),
+    //score: Int,
+    //alreadyPracticedWordsIDs: String
+) {
+    val allWords by wordsViewModel.allWords.observeAsState(listOf())
+    var wordsList = allWords
+    MultipleChoiceScreen(
+        wordsList = wordsList,
+        navController = navController,
+        //score = score,
+        //alreadyPracticedWordsIDs = alreadyPracticedWordsIDs,
+    )
+}
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MultipleChoiceScreen(
-    navController: NavHostController
+    navController: NavHostController,
+    wordsList: List<Word> = listOf(),
 ) {
     Column(
         modifier = Modifier
@@ -48,78 +67,73 @@ fun MultipleChoiceScreen(
                     .fillMaxWidth()
                     .padding(start = 8.dp, end = 8.dp, top = 8.dp),
             ) {
-                Surface(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(0.dp),
-                    shadowElevation = 3.dp,
-                    shape = RoundedCornerShape(11.dp),
-                    onClick = { /*TODO*/ },
-                ) {
-                    Text(
-                        text = "What is the meaning of life?",
-                        modifier = Modifier
-                            .padding(16.dp),
-                        fontSize = 20.sp
-                    )
-                }
-                Spacer(modifier = Modifier.height(20.dp))
-                Surface(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        //.shadow(1.dp, RoundedCornerShape(11.dp))
-                        .padding(6.dp),
-                    shadowElevation = 3.dp,
-                    shape = RoundedCornerShape(11.dp),
-                ) {
-                    Text(
-                        text = "A. Love",
-                        modifier = Modifier.padding(16.dp)
-                    )
-                }
-                Surface(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        //.shadow(1.dp, RoundedCornerShape(11.dp))
-                        .padding(6.dp),
-                    shadowElevation = 3.dp,
-                    shape = RoundedCornerShape(11.dp),
-                ) {
-                    Text(
-                        text = "B. Not Love",
-                        modifier = Modifier.padding(16.dp)
-                    )
-                }
-                Surface(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        //.shadow(1.dp, RoundedCornerShape(11.dp))
-                        .padding(6.dp),
-                    shadowElevation = 3.dp,
-                    shape = RoundedCornerShape(11.dp),
-                ) {
-                    Text(
-                        text = "C. Maybe Love",
-                        modifier = Modifier.padding(16.dp)
-                    )
-                }
-                Surface(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        //.shadow(1.dp, RoundedCornerShape(11.dp))
-                        .padding(6.dp),
-                    shadowElevation = 3.dp,
-                    shape = RoundedCornerShape(11.dp),
-                ) {
-                    Text(
-                        text = "D. Pomegranate",
-                        modifier = Modifier.padding(16.dp)
-                    )
-                }
-                Spacer(modifier = Modifier.height(12.dp))
+                MakeQuestion()
             }
         }
     }
+}
+
+@Composable
+fun MakeQuestion(
+    shuffledWordList: List<Word> = GetShuffledList()
+){
+    val randomList
+    for (i in 0..3){
+
+    }
+
+    val randomList = mutableSetOf(IntRange(1, shuffledWordList.size).random())
+    randomList.add(0)
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth(),
+        shadowElevation = 3.dp,
+        shape = RoundedCornerShape(11.dp),
+        //onClick = { /*TODO*/ },
+    ) {
+        Text(
+            text = shuffledWordList[0].word,
+            modifier = Modifier
+                .padding(16.dp),
+            fontSize = 20.sp
+        )
+    }
+    Spacer(modifier = Modifier.height(20.dp))
+    QuestionChoice(choice = shuffledWordList[1].translation, onClick = { /*TODO*/ })
+    QuestionChoice(choice = shuffledWordList[2].translation, onClick = { /*TODO*/ })
+    QuestionChoice(choice = shuffledWordList[3].translation, onClick = { /*TODO*/ })
+    QuestionChoice(choice = shuffledWordList[4].translation, onClick = { /*TODO*/ })
+    Spacer(modifier = Modifier.height(12.dp))
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun QuestionChoice(
+    choice: String,
+    onClick: () -> Unit
+){
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(6.dp),
+        shadowElevation = 3.dp,
+        shape = RoundedCornerShape(11.dp),
+        onClick = onClick,
+    ) {
+        Text(
+            text = choice,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
+
+@Composable
+fun GetShuffledList(
+    wordsList: List<Word> = listOf(),
+): List<Word> {
+    return wordsList.shuffled()
 }
 
 
